f4070d1df0ae011215da5e0aae9dcfd9e851b048
osman34 <osman34@schule.bremen.de>
2022-03-21 11:02:18 +0100

    21.03

--- Lernsituation/Chatbot.py
+++ Lernsituation/Chatbot.py
@@ -1,5 +1,4 @@
 import random
-import string
 
 
 def welcome():
@@ -14,7 +13,7 @@ def namen_abfrage():
 
 
 def benutzeranleitung():
-    print(name, ",", "Wenn du Hilfe benötigst hier:")
+    print(name, ",", "wenn du Hilfe benötigst hier:")
     print("Beenden  | Zum beenden musst du einfach 'bye' eintippen")
     print("Frage    | Zum fragen musst du einfach 'frage' eintippen")
     print("Leer     | Zum wiederholen der Eingabe musst du einfach nichts eintippen")
@@ -22,11 +21,42 @@ def benutzeranleitung():
 
 
 zufallsantworten = ["Weiß ich nicht...", "Leider kann ich dir das nicht beantworten ", "Interesannt... "
-                                                                                        "Kannst du das anders formulieren?",
+                                                                                        "Kannst du das anders "
+                                                                                        "formulieren?",
                     "Das habe ich leider nicht verstanden..."]
+antworten = {"strom": "Ich liebe Strom!",
+             "bist du": "Ich eine eine KI",
+             "hallo": "Hi!",
+             "dich programmiert": "Mein programmierer heißt Osman",
+             "entsperre account": "Melde dich hierfür bitte beim Support Level '2'",
+             "wie lange unternehmen": "Unser Unternehmen gibt es seit ca. 5 Jahren",
+             "bye": "Tschüss! ich hoffe ich konnte dir weiterhelfen",
+             "alt": "Ich bin drei Wochen alt"}
+
+
+def passende_antworten():
+    if "strom" in thema:
+        print(antworten["strom"])
+    elif "bist du" in thema:
+        print(antworten["bist du"])
+    elif "hallo" in thema:
+        print(antworten["hallo"])
+    elif "dich programmiert" in thema:
+        print(antworten["dich programmiert"])
+    elif "entsperre account" in thema:
+        print(antworten["entsperre account"])
+    elif "wie lange unternehmen" in thema:
+        print(antworten["wie lange unternehmen"])
+    elif "bye" in thema:
+        print(antworten["bye"])
+    elif "alt" in thema:
+        print(antworten["alt"])
+    else:
+        print(random.choice(zufallsantworten))
+
 
 welcome()  # Willkommens Text
-namen_abfrage()  # Name wird gedruckt
+namen_abfrage()  # Name wird gefragt
 print("")
 benutzeranleitung()  # Benutzeranleitung
 print("")
@@ -39,14 +69,14 @@ while True:
         thema = input("Worüber möchtest du heute sprechen?: ")
         thema = thema.lower()
         thema = thema.split()
-        print(random.choice(zufallsantworten))
+        passende_antworten()
     elif eingabe == "" or eingabe == " ":
         optionen = input("Ungültige Eingabe - Sie haben zwei Möglichkeiten\n1. Frage oder 2. Benutzeranleitung\n")
         if optionen == "1":
             thema = input("Worüber möchtest du heute sprechen?\n")
             thema = thema.lower()
             thema = thema.split()
-            print(random.choice(zufallsantworten))
+            passende_antworten()
         elif optionen == "2":
             benutzeranleitung()
         else:
